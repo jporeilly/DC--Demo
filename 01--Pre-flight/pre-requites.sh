c990b1bf05efd65ed6735814b667d524fe69ff08
James O'Reilly <jp.oreilly@hotmail.com>
2022-04-25 18:14:49 +0100

    Updates

--- 01--Pre-flight/pre-requites.sh
+++ 01--Pre-flight/pre-requites.sh
@@ -12,34 +12,28 @@
 #                      software-properties-common
 # Install Docker
 # Install Docker Compose 
-# Install Harbor with Chartmuseum: 
-#                     Comment out HTTPS section.
+#
 # 24/04/2022
 # ==============================================================
 
-# Prompt for the user to ask if the install should use the IP Address or Fully Qualified Domain Name of the Harbor Server
-PS3='Would you like to install Harbor based on IP or FQDN? '
-select option in IP FQDN
-do
-    case $option in
-        IP)
-            IPorFQDN=$(hostname -I|cut -d" " -f 1)
-            break;;
-        FQDN)
-            IPorFQDN=$(hostname -f)
-            break;;
-     esac
-done
-
 # Infrastructure
-apt update -y
+apt update && upgrade -y
 swapoff --all
 sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 ufw disable # Do not disable in Production.
 echo -e "Infrastructure update completed .."
 
-# Install Latest Stable Docker Release
+# Install Jq
+apt-get install -y jq
+
+# Install Helm
+snap install helm --classic
+
+# Docker pre-requisites
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+echo -e "Pre-requisites installation completed .."
+
+# Install Latest Stable Docker Release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
@@ -47,7 +41,7 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 tee /etc/docker/daemon.json >/dev/null <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
-  "insecure-registries" : ["$IPorFQDN:443","$IPorFQDN:80","0.0.0.0/0"],
+  "insecure-registries" : ["localhost:5000","0.0.0.0/0"],
   "log-driver": "json-file",
   "log-opts": {
     "max-size": "100m"
@@ -61,7 +55,9 @@ MAINUSER=$(logname)
 usermod -aG docker $MAINUSER
 systemctl daemon-reload
 systemctl restart docker
-echo -e "Docker Installation complete .."
+echo -e "Docker Installation completed .."
+
+
 
 # Install Latest Stable Docker Compose Release
 COMPOSEVERSION=$(curl -s https://github.com/docker/compose/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
@@ -70,20 +66,3 @@ chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "Docker Compose Installation completed .."
 
-# Install Latest Stable Harbor Release
-HARBORVERSION=$(curl -s https://github.com/goharbor/harbor/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
-curl -s https://api.github.com/repos/goharbor/harbor/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
-tar xvf harbor-online-installer-v$HARBORVERSION.tgz
-
-cd harbor
-cp harbor.yml.tmpl harbor.yml
-# comment out https
-sed -i "s/reg.mydomain.com/$IPorFQDN/g" harbor.yml
-sed -e '/port: 443$/ s/^#*/#/' -i harbor.yml
-sed -e '/https:$/ s/^#*/#/' -i harbor.yml
-sed -e '/\/your\/certificate\/path$/ s/^#*/#/' -i harbor.yml
-sed -e '/\/your\/private\/key\/path$/ s/^#*/#/' -i harbor.yml
-
-mkdir /var/log/harbor
-./install.sh --with-chartmuseum
-echo -e "Harbor Installation Completed .. \n\nPlease log out and log in or run the command 'newgrp docker' to use Docker without sudo\n\nLogin to your harbor instance:\n docker login -u admin -p Harbor12345 $IPorFQDN"
