47a884cf215a4db8e03a5ad7eb978f27faec93f1
James O'Reilly <jp.oreilly@hotmail.com>
2022-06-16 14:36:09 +0100

    Update deploy_k3s.sh

--- 01--Pre-flight/deploy_k3s.sh
+++ 01--Pre-flight/deploy_k3s.sh
@@ -11,16 +11,13 @@
 curl https://get.k3s.io | sudo INSTALL_K3S_COMMIT=$COMMIT INSTALL_K3S_TYPE=server sh -s - --cluster-init --disable traefik
 
 # Connect and test kubectl
-mkdir ~/.kube
-chown -R $(id -u) /etc/rancher/k3s
+mkdir $HOME/.kube
+chown -R $(id -u) /etc/rancher/k3s/*
 cp -i /etc/rancher/k3s/k3s.yaml  $HOME/.kube/config
-chown -R  $(id -u):$(id -g) $HOME/.kube/
+chown -R  $(id -u):$(id -g) $HOME/.kube/*
 systemctl enable k3s
 sleep 5s
-kubectl get nodes # note kubernetes version
-sleep 10s
 echo -e "k3s Installation Completed .."
-
 sleep 5s
 echo -e "Reboot required .."
 reboot
