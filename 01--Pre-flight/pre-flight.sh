d057ad712ac17e50688225708136497d8ad61ac5
James O'Reilly <jp.oreilly@hotmail.com>
2022-06-22 12:13:09 +0100

    Update pre-flight.sh

--- 01--Pre-flight/pre-flight.sh
+++ 01--Pre-flight/pre-flight.sh
@@ -20,9 +20,6 @@ dnf install epel-release -y
 swapoff --all
 sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 systemctl disable firewalld # Do not disable in Production.
-cp /etc/sysctl.conf /etc/sysctl.conf.bak # Elasticsearch requires a max map count > 262144
-chown -R $(id -u) /etc/sysctl.conf
-echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 sysctl -p --system
 echo -e "Infrastructure update completed .."
 
